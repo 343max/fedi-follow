84d76ba9798a4e261165e61340eb225cb6617fc9
Max von Webel <max@343max.de>
2024-11-19 18:20:53 +0100

    picker for instances

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from "react"
-import "./App.css"
-import { ProfileConverter } from "@/components/profileConverter"
+import React from "react";
+import "./App.css";
+import { ProfileConverter } from "@/components/profileConverter";
 
 function App() {
-  return (
-    <div className="App">
-      <ProfileConverter />
-    </div>
-  )
+	return (
+		<div className="App dark:bg-zinc-900 bg-zinc-100">
+			<ProfileConverter />
+		</div>
+	);
 }
 
-export default App
+export default App;
--- src/components/profileConverter/index.tsx
+++ src/components/profileConverter/index.tsx
@@ -10,13 +10,17 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { ComboboxDemo } from "../instanceCombobox";
+import { mastodonInstances } from "@/lib/mastodonInstances";
 import { Button } from "../ui/button";
 
 export const ProfileConverter = () => {
 	const [instance, setInstance] = React.useState("");
+	const instanceList = React.useMemo(
+		() => mastodonInstances.filter((i) => i.startsWith(instance)).slice(0, 10),
+		[instance],
+	);
 	return (
-		<Card className="w-[350px]">
+		<Card className="w-[500px]">
 			<CardHeader>
 				<CardTitle>Follow on Mastodon</CardTitle>
 				<CardDescription>
@@ -36,7 +40,18 @@ export const ProfileConverter = () => {
 						</div>
 						<div className="flex flex-col space-y-1.5">
 							<Label htmlFor="framework">Your Mastodon Instance</Label>
-							<ComboboxDemo value={instance} setValue={setInstance} />
+							<Input
+								id="instance"
+								list="instance-list"
+								placeholder="mastodon.social"
+								value={instance}
+								onChange={(e) => setInstance(e.target.value)}
+							/>
+							<datalist id="instance-list">
+								{instanceList.map((instance) => (
+									<option key={instance} value={instance} />
+								))}
+							</datalist>
 						</div>
 					</div>
 				</form>
