f8f6179beb735ddaee0a924ee5db008b0a08507d
Max von Webel <max@343max.de>
2024-11-21 14:17:33 +0100

    fixed import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { ProfileBridger } from "@/components/profileConverter";
+import { ProfileBridger } from "@/components/profileBridger";
 
 function App() {
 	return (
