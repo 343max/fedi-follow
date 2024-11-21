05c66cede6391a9c2cb98cd5e53b90f95b7129a7
Max von Webel <max@343max.de>
2024-11-21 20:34:34 +0100

    state

--- src/components/profileBridger/profileLinkInput.tsx
+++ src/components/profileBridger/profileLinkInput.tsx
@@ -1,11 +1,32 @@
 import { Input } from "app/components/ui/input";
 import { Label } from "app/components/ui/label";
+import React from "react";
+import { Button } from "../ui/button";
 
 export const ProfileLinkInput = () => {
+	const [url, setUrl] = React.useState("");
+
+	React.useEffect(() => {
+		const hashParams = document.location.hash.slice(1);
+		const params = new URLSearchParams(hashParams);
+		const profileUrl = params.get("profileUrl");
+		if (profileUrl) {
+			setUrl(profileUrl);
+		}
+	}, []);
+
 	return (
-		<div className="flex flex-col space-y-1.5">
+		<div className="flex flex-col space-y-1.5 gap-2">
 			<Label htmlFor="name">Profile Link</Label>
-			<Input id="name" placeholder="https://bsky.app/profile/343max.de" />
+			<Input
+				id="name"
+				value={url}
+				onChange={(e) => setUrl(e.target.value)}
+				placeholder="https://bsky.app/profile/343max.de"
+			/>
+			<div className="flex justify-end">
+				<Button>Check URL</Button>
+			</div>
 		</div>
 	);
 };
--- src/lib/convertUrl.test.ts
+++ src/lib/convertUrl.test.ts
@@ -1,18 +1,21 @@
-import { extractMastodonHandle } from "./convertUrl"
+import { describe, expect, test } from "bun:test";
+import { extractMastodonHandle } from "./convertUrl";
 
 describe("extractMastodonHandle", () => {
-  test("bluesky profile url", () => {
-    const handle = extractMastodonHandle("https://bsky.app/profile/343max.de")
-    expect(handle).toEqual({ host: "bsky.brid.gy", handle: "343max.de" })
-  })
+	test("bluesky profile url", () => {
+		const handle = extractMastodonHandle("https://bsky.app/profile/343max.de");
+		expect(handle).toEqual({ host: "bsky.brid.gy", handle: "343max.de" });
+	});
 
-  test("threads profile url", () => {
-    const handle = extractMastodonHandle("https://www.threads.net/@anildash")
-    expect(handle).toEqual({ host: "threads.net", handle: "anildash" })
-  })
+	test("threads profile url", () => {
+		const handle = extractMastodonHandle("https://www.threads.net/@anildash");
+		expect(handle).toEqual({ host: "threads.net", handle: "anildash" });
+	});
 
-  test("random url", () => {
-    const handle = extractMastodonHandle("https://www.theverge.com/24295933/bluesky-social-network-custom-how-to")
-    expect(handle).toBeNull()
-  })
-})
+	test("random url", () => {
+		const handle = extractMastodonHandle(
+			"https://www.theverge.com/24295933/bluesky-social-network-custom-how-to",
+		);
+		expect(handle).toBeNull();
+	});
+});
