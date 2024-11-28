3c3cafe47b1b35a6df2e537cd5f900c7328e8a0c
Max von Webel <max@343max.de>
2024-11-28 15:36:20 +0100

    better fragments listener

--- src/hooks/useFragmentParams.ts
+++ src/hooks/useFragmentParams.ts
@@ -2,11 +2,19 @@ import { useEffect, useState } from "react";
 
 export const useFragmentParams = (): Record<string, string> => {
 	const [params, setParams] = useState<Record<string, string>>({});
-	const hash = document.location.hash;
 	useEffect(() => {
-		const params = new URLSearchParams(hash.slice(1));
-		setParams(Object.fromEntries(params.entries()));
-	}, [hash]);
+		const listener = () => {
+			const hash = document.location.hash;
+			const params = new URLSearchParams(hash.slice(1));
+			setParams(Object.fromEntries(params.entries()));
+		};
+
+		window.addEventListener("hashchange", listener);
+
+		return () => {
+			window.removeEventListener("hashchange", listener);
+		};
+	}, []);
 
 	return params;
 };
