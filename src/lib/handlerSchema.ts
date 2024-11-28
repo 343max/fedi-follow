f0716e542fa283f8a04734b37ca60166e6c8c340
Max von Webel <max@343max.de>
2024-11-28 19:17:46 +0100

    verifying instance urls

--- src/lib/handlerSchema.ts
+++ src/lib/handlerSchema.ts
@@ -1,7 +1,20 @@
 import { z } from "zod";
 
 export const handlerSchema = z.discriminatedUnion("kind", [
-	z.object({ kind: z.literal("webbrowser"), instance: z.string() }),
+	z.object({
+		kind: z.literal("webbrowser"),
+		instance: z
+			.string()
+			.refine(
+				(s) =>
+					s.match(
+						/^((?!-))(xn--)?[a-z0-9][a-z0-9-_]{0,61}[a-z0-9]{0,1}\.(xn--)?([a-z0-9\-]{1,61}|[a-z0-9-]{1,30}\.[a-z]{2,})$/,
+					),
+				{
+					message: "Please enter the domain of your instance",
+				},
+			),
+	}),
 	z.object({
 		kind: z.literal("app"),
 		app: z.string(),
