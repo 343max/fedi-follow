19c56a7e518cafecb584319252c2b3d1eb3b0926
Max von Webel <max@343max.de>
2024-11-28 15:53:57 +0100

    fixed fragment handling

--- src/hooks/useFragmentParams.ts
+++ src/hooks/useFragmentParams.ts
@@ -1,20 +1,25 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export const useFragmentParams = (): Record<string, string> => {
 	const [params, setParams] = useState<Record<string, string>>({});
-	useEffect(() => {
-		const listener = () => {
-			const hash = document.location.hash;
-			const params = new URLSearchParams(hash.slice(1));
-			setParams(Object.fromEntries(params.entries()));
-		};
 
-		window.addEventListener("hashchange", listener);
+	const parseHash = useCallback(() => {
+		const hash = document.location.hash;
+		const params = new URLSearchParams(hash.slice(1));
+		setParams(Object.fromEntries(params.entries()));
+	}, []);
+
+	useEffect(() => {
+		parseHash();
+	}, [parseHash]);
+
+	useEffect(() => {
+		window.addEventListener("hashchange", parseHash);
 
 		return () => {
-			window.removeEventListener("hashchange", listener);
+			window.removeEventListener("hashchange", parseHash);
 		};
-	}, []);
+	}, [parseHash]);
 
 	return params;
 };
