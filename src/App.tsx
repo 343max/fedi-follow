146ddd2eed1610ced9a22be90ab9f08158cc59cb
Max von Webel <max@343max.de>
2024-11-28 14:17:36 +0100

    no more App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import "./App.css";
 import { ProfileBridger } from "@app/components/profileBridger";
 
 function App() {
 	return (
-		<div className="App dark:bg-zinc-900 bg-zinc-100">
+		<div className="dark:bg-zinc-900 bg-zinc-100 p-3 h-[100vh] flex justify-center items-center">
 			<ProfileBridger />
 		</div>
 	);
